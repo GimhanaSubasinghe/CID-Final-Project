42baa3cf44b110e1c6cf60fbfb6912be47a73138
Gimhana Subasinghe <120109602+GimhanaSubasinghe@users.noreply.github.com>
2025-02-09 11:39:50 +0530

    Update config.php

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 $host = "sltclothing.c0z404e2a7r7.us-east-1.rds.amazonaws.com"; // Update with actual RDS endpoint
 $username = "admin";
 $password = "sltclothing12345";
-$database = "sltclothing"; // Your actual database name
+$database = "slt_clothing"; // Your actual database name
 
 $conn = new mysqli($host, $username, $password, $database);
 
